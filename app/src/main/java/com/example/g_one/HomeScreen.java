f482b4991a5b8503e0b25593317628fc31371c40
Avijit Das <avijit@Avijits-MacBook-Air.local>
2020-01-22 11:40:11 +0530

    my first final commit

---
+++ app/src/main/java/com/example/g_one/HomeScreen.java
@@ -0,0 +1,50 @@
+package com.example.g_one;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.firebase.auth.FirebaseAuth;
+
+public class HomeScreen extends AppCompatActivity {
+
+    String number;
+    Button b1,b2,b3,b4,b5;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home_screen);
+        final Intent intent = getIntent();
+        String suburb = intent.getStringExtra("number");
+        number = suburb;
+        b1=(Button)findViewById(R.id.med_hist);
+        b2=(Button)findViewById(R.id.logout);
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i  = new Intent(HomeScreen.this,MedHistory.class);
+                i.putExtra("number",number);
+                startActivity(i);
+            }
+        });
+        b2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FirebaseAuth.getInstance().signOut();
+                Intent i = new Intent(HomeScreen.this,MainActivity.class);
+                startActivity(i);
+            }
+        });
+    }
+
+    @Override
+    public void onBackPressed() {
+        Intent a = new Intent(Intent.ACTION_MAIN);
+        a.addCategory(Intent.CATEGORY_HOME);
+        a.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(a);
+    }
+}
